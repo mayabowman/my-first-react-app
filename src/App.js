8a0bdd49c448dba4b8ca7f3f7998efa506c85638
Maya Bowman <mayashady1@gmail.com>
2019-08-25 10:46:14 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import moment from 'moment';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Let's change some stuff!
         </p>
         <a
           className="App-link"
